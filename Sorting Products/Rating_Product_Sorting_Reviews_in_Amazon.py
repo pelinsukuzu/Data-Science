feb03a68535f0b16b7c1b9df18c0b5211f437d5b
Pelinsu Kuzu <112900210+pelinsukuzu@users.noreply.github.com>
2025-03-14 22:10:40 +0300

    Add files via upload

---
+++ Sorting Products/Rating_Product_Sorting_Reviews_in_Amazon.py
@@ -0,0 +1,97 @@
+##################################################
+# PROJE: Rating Product & Sorting Reviews in Amazon
+###################################################
+
+###################################################
+# İş Problemi
+###################################################
+
+# E-ticaretteki en önemli problemlerden bir tanesi ürünlere satış sonrası verilen puanların doğru şekilde hesaplanmasıdır.
+# Bu problemin çözümü e-ticaret sitesi için daha fazla müşteri memnuniyeti sağlamak, satıcılar için ürünün öne çıkması ve satın
+# alanlar için sorunsuz bir alışveriş deneyimi demektir. Bir diğer problem ise ürünlere verilen yorumların doğru bir şekilde sıralanması
+# olarak karşımıza çıkmaktadır. Yanıltıcı yorumların öne çıkması ürünün satışını doğrudan etkileyeceğinden dolayı hem maddi kayıp
+# hem de müşteri kaybına neden olacaktır. Bu 2 temel problemin çözümünde e-ticaret sitesi ve satıcılar satışlarını arttırırken müşteriler
+# ise satın alma yolculuğunu sorunsuz olarak tamamlayacaktır.
+
+###################################################
+# Veri Seti Hikayesi
+###################################################
+
+# Amazon ürün verilerini içeren bu veri seti ürün kategorileri ile çeşitli metadataları içermektedir.
+# Elektronik kategorisindeki en fazla yorum alan ürünün kullanıcı puanları ve yorumları vardır.
+
+# Değişkenler:
+# reviewerID: Kullanıcı ID’si
+# asin: Ürün ID’si
+# reviewerName: Kullanıcı Adı
+# helpful: Faydalı değerlendirme derecesi
+# reviewText: Değerlendirme
+# overall: Ürün rating’i
+# summary: Değerlendirme özeti
+# unixReviewTime: Değerlendirme zamanı
+# reviewTime: Değerlendirme zamanı Raw
+# day_diff: Değerlendirmeden itibaren geçen gün sayısı
+# helpful_yes: Değerlendirmenin faydalı bulunma sayısı
+# total_vote: Değerlendirmeye verilen oy sayısı
+
+import pandas as pd
+import datetime as dt
+import math
+import scipy.stats as st
+from sklearn.preprocessing import MinMaxScaler
+
+pd.set_option('display.expand_frame_repr',False)
+pd.set_option('display.max_columns',None)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+
+
+df_ = pd.read_csv("/Users/pelinsukuzu/Desktop/Data Science Bootcamp/Ölçümleme Problemleri/Case Study 1 /amazon_review.csv")
+
+df = df_.copy()
+
+###################################################
+# GÖREV 1: Average Rating'i Güncel Yorumlara Göre Hesaplayınız ve Var Olan Average Rating ile Kıyaslayınız.
+###################################################
+
+# Paylaşılan veri setinde kullanıcılar bir ürüne puanlar vermiş ve yorumlar yapmıştır.
+# Bu görevde amacımız verilen puanları tarihe göre ağırlıklandırarak değerlendirmek.
+# İlk ortalama puan ile elde edilecek tarihe göre ağırlıklı puanın karşılaştırılması gerekmektedir.
+
+
+###################################################
+# Adım 1: Veri Setini Okutunuz ve Ürünün Ortalama Puanını Hesaplayınız.
+###################################################
+
+df.head()
+df["overall"].mean
+###################################################
+# Adım 2: Tarihe Göre Ağırlıklı Puan Ortalamasını Hesaplayınız.
+###################################################
+current_day = dt.datetime.now()
+df["days"] = (current_day-df["reviewTime"]).dt.days
+
+
+###################################################
+# Görev 2: Ürün için Ürün Detay Sayfasında Görüntülenecek 20 Review'i Belirleyiniz.
+###################################################
+
+
+###################################################
+# Adım 1. helpful_no Değişkenini Üretiniz
+###################################################
+
+# Not:
+# total_vote bir yoruma verilen toplam up-down sayısıdır.
+# up, helpful demektir.
+# veri setinde helpful_no değişkeni yoktur, var olan değişkenler üzerinden üretilmesi gerekmektedir.
+
+
+###################################################
+# Adım 2. score_pos_neg_diff, score_average_rating ve wilson_lower_bound Skorlarını Hesaplayıp Veriye Ekleyiniz
+###################################################
+
+
+##################################################
+# Adım 3. 20 Yorumu Belirleyiniz ve Sonuçları Yorumlayınız.
+###################################################
+
